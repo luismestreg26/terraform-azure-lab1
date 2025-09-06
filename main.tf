f88d33cce35dfcfd07511c3c0a2a2c86c31aa4cc
luismestreg26 <luismestre_26@hotmail.com>
2025-09-06 12:25:21 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,30 @@
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "vm-lab"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = var.admin_username
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = var.ssh_public_key
+  }
+
+  disable_password_authentication = true
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+}
