f85325e41f9bce9d12699c891b001b002d82ba64
luismestreg26 <luismestre_26@hotmail.com>
2025-09-06 12:54:06 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,3 +28,46 @@ resource "azurerm_linux_virtual_machine" "vm" {
     version   = "latest"
   }
 }
+
+#Crear IP Pública, NSG y asignarlo a la VM
+
+resource "azurerm_public_ip" "vm_ip" {
+  name                = "pip-${var.vm_name}"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg-${var.vm_name}"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+resource "azurerm_network_interface_security_group_association" "nic_nsg_assoc" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+variable "vm_name" {
+  description = "The name for the virtual machine and related resources."
+  type        = string
+  default     = "lab-vm"  # Puedes poner un valor por defecto aquí
+}
+
+variable "location" {
+  description = "The Azure region where resources will be created."
+  type        = string
+  default     = "East US 2" # Puedes usar la misma ubicación que tu grupo de recursos
+}
